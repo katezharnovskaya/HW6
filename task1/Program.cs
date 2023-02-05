fda2dd4e63c857bcbf0e65ce6785ed1e6f8802dd
Ekaterina <firegirl-83@mail.ru>
2023-02-05 20:25:47 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 41
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите элементы через запятую: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int count = array.Count(x => x >0); 
+Console.WriteLine($"Количество элементов больше ноля: {count}");
+
+
